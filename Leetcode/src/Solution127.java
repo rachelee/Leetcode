631b51eeaf85658fd8e6f4b82555cb9454b5d51d
rachelee <lixiaox5@gmail.com>
2015-06-24 23:44:43 -0700

    Solution127 and 53

---
+++ Leetcode/src/Solution127.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+
+
+public class Solution127 {
+	public static int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(beginWord);
+        int queueLen = 1; // last queue length
+        int curLen = 0; // current queue length
+        int level = 1;
+        while(!queue.isEmpty())
+        {
+                String word = queue.poll();
+                queueLen--;
+                    
+                char[] wordArr = word.toCharArray();
+                for(int i = 0; i < wordArr.length; i++)
+                {
+                    for(char c = 'a'; c <= 'z'; c++)
+                    {
+                    	
+                        if(wordArr[i]!=c)
+                        {
+                            char temp = wordArr[i];
+                            wordArr[i] = c;
+                            String newWord =new String(wordArr);
+                            //System.out.println(newWord);
+                            if(newWord.equals(endWord))
+                            {
+                                return level+1;
+                            }
+                            if(wordDict.contains(newWord))
+                            {
+                                queue.add(newWord);
+                                curLen++;
+                                wordDict.remove(newWord);
+                            }
+                            wordArr[i] = temp;
+                        }
+                        
+                    }
+                    
+                }
+                if(queueLen == 0)
+                {
+                    queueLen = curLen;
+                    curLen = 0;
+                    level++;
+                }
+        }
+        return 0;
+    }
+    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a  = "hot";
+		String b = "dog";
+		Set<String> arr = new HashSet<>();
+		arr.add("hot");
+		arr.add("dog");
+		arr.add("dot");
+		System.out.println(ladderLength(a, b, arr));
+	}
+
+}
--- Leetcode/src/Solution53.java
+++ Leetcode/src/Solution53.java
@@ -1,6 +1,19 @@
 
 public class Solution53 {
-
+	public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+        int maxNum = res[0];
+        for(int i = 1; i < nums.length; i++)
+        {
+            res[i] = Math.max(nums[i], nums[i] + res[i-1]);
+            maxNum = Math.max(res[i], maxNum);
+        }
+        
+        return maxNum;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
